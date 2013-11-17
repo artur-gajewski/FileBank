d520d67c756f2bb3677fddee28e43fb2c52a1bc3
Florian Levis <levis.florian@gmail.com>
2013-11-17 13:04:49 +0100

    FIX Create module Exception (like all zf2 components : ExceptionInterface + Extends of default Exception) FIX Generate autoload_classmap

---
+++ src/FileBank/Exception/ExceptionInterface.php
@@ -0,0 +1,6 @@
+<?php
+namespace FileBank\Exception;
+
+interface ExceptionInterface
+{
+}
---
+++ src/FileBank/Exception/RuntimeException.php
@@ -0,0 +1,6 @@
+<?php
+namespace FileBank\Exception;
+
+class RuntimeException extends \RuntimeException implements ExceptionInterface
+{
+}
