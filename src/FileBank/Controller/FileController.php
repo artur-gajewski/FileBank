273fa3fc5bb7ad38fbc3b544303177ee4ab20192
Levis Florian <levis.florian@gmail.com>
2014-08-12 23:00:09 +0200

    Update Controller
    Update FileController to use Http\Response

--- src/FileBank/Controller/FileController.php
+++ src/FileBank/Controller/FileController.php
@@ -1,34 +1,51 @@
 <?php
-
 namespace FileBank\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
-use Zend\View\Model\ViewModel;
+use SplFileInfo;
+use DateTime;
 
-class FileController extends AbstractActionController 
+use Zend\Http;
+use Zend\Mvc\Controller\AbstractActionController;
+
+use FileBank\Exception\RuntimeException;
+
+class FileController extends AbstractActionController
 {
     /**
-     * Get the file from FileBank and offer it for download 
+     * Get the file from FileBank and offer it for download
      */
-    public function downloadAction() 
+    public function downloadAction()
     {
+        /* @var $filelib \FileBank\Manager */
         $filelib = $this->getServiceLocator()->get('FileBank');
         $id = (int) $this->getEvent()->getRouteMatch()->getParam('id');
 
+        // Will throw 404 error if file not found
         $file = $filelib->getFileById($id);
-        $filePath = $filelib->getRoot() . $file->getSavePath();
 
-        header('Content-Description: File Transfer');
-        header('Content-Type: ' . $file->getMimetype());
-        header('Content-Disposition: attachment; filename=' . $file->getName());
-        header('Content-Transfer-Encoding: binary');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . $file->getSize());
-        ob_clean();
-        flush();
-        readfile($filePath);
-        exit;
+        $fileInfo = new SplFileInfo($filelib->getRoot() . $file->getSavePath());
+        if (!$fileInfo->isFile() || !$fileInfo->isReadable()) {
+            throw new RuntimeException('Cannot read file');
+        }
+
+        $response = new Http\Response\Stream();
+        // SplFile are not resources :(
+        $response->setStream(fopen($fileInfo->getRealPath(), 'r'))
+            ->setStatusCode(Http\Response::STATUS_CODE_200);
+
+        $expires = new DateTime();
+        $expires->setTimestamp(0);
+
+        $response->getHeaders()
+            ->addHeaderLine('Content-Type', $file->getMimetype())
+            ->addHeaderLine('Content-Disposition', 'attachment; filename=' . $file->getName())
+            ->addHeaderLine('Content-Transfer-Encoding', 'binary')
+            ->addHeaderLine('Content-Length', $fileInfo->getSize())
+            // Cache
+            ->addHeaderLine('Cache-Control', 'must-revalidate')
+            ->addHeaderLine('Expires', $expires->format(DateTime::RFC1123))
+            ->addHeaderLine('Pragma', 'public');
+
+        return $response;
     }
 }
